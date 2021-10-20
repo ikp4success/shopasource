d6551618773e640c35c1dd68b234dab24fbcabee
Immanuel George <ikp4success@yahoo.com>
2021-10-20 05:24:02 -0400

    check api usage limit

--- webapp/auth.py
+++ webapp/auth.py
@@ -18,9 +18,15 @@ def authorize(app):
                 k = k.lower()
                 headers_lower_case[k] = v
             auth = headers_lower_case.get("x-api-key")
-            api_key = app.config.get("API_KEY")
+            # check api usage limit
+            api_key_info = get_api_key(user=request.remote_addr)
+            if api_key_info.get("error"):
+                return api_key_info, 429
 
-            if not api_key or auth != api_key:
+            if (
+                not api_key_info.get("public_api_key")
+                or auth != api_key_info["public_api_key"]
+            ):
                 logger.error("User could not authenticate!")
                 status = {
                     "status": "Unauthorized",
@@ -30,10 +36,6 @@ def authorize(app):
                     status,
                     401,
                 )
-            # check api usage limit
-            api_key_info = get_api_key(user=request.remote_addr)
-            if api_key_info.get("error"):
-                return api_key_info, 429
             return await func(*args, **kwargs)
 
         return wrapper
